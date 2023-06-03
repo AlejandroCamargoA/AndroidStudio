33fc1c9e511ca1dea7896aa2c46d4932fc13d2cd
AlejandroCamargoA <20211D115@khipu.edu.pe>
2023-06-02 19:36:15 -0500

    Examen - Calculadora

--- Calculadora/app/src/main/java/com/example/ejercicio07_calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/ejercicio07_calculadora/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlin.math.sqrt
 
 class MainActivity : AppCompatActivity() {
+
+    //Declaracion de variables globales
     lateinit var lblPantalla : TextView;
     lateinit var btn0: Button;
     lateinit var btn1: Button;
@@ -29,9 +31,9 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnRaiz: Button;
     lateinit var btnInversa: Button;
     lateinit var btnPorcentaje: Button;
-
     lateinit var btnIgual: Button;
 
+    //Declaracion de variables globales auxiliares
     var num1:Double = 0.0
     var num2:Double = 0.0
     var operacion: Int = 0
@@ -40,6 +42,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Agregar objetos a las variables
         lblPantalla = findViewById(R.id.lblPantalla);
         btn0 = findViewById(R.id.btn0);
         btn1 = findViewById(R.id.btn1);
@@ -61,9 +64,9 @@ class MainActivity : AppCompatActivity() {
         btnRaiz = findViewById(R.id.btnRaiz)
         btnInversa = findViewById(R.id.btnInversa)
         btnPorcentaje = findViewById(R.id.btnPorcentaje)
-
         btnIgual = findViewById(R.id.btnIgual)
 
+        //Listener para distintas acciones en los botones
         btn0.setOnClickListener{ agregarNum("0") }
         btn1.setOnClickListener{ agregarNum("1") }
         btn2.setOnClickListener{ agregarNum("2") }
@@ -74,36 +77,39 @@ class MainActivity : AppCompatActivity() {
         btn7.setOnClickListener{ agregarNum("7") }
         btn8.setOnClickListener{ agregarNum("8") }
         btn9.setOnClickListener{ agregarNum("9") }
-
         btnLimpiar.setOnClickListener{ limpiar() }
-
         btnPunto.setOnClickListener{ agregarPunto() }
-
         btnMasMenos.setOnClickListener{ cambiarSigno() }
-
         btnSuma.setOnClickListener { Operacion(1) }
         btnResta.setOnClickListener{ Operacion(2) }
         btnMultiplicacion.setOnClickListener{ Operacion(3) }
         btnDivision.setOnClickListener{ Operacion(4) }
+        btnPorcentaje.setOnClickListener{ Operacion(5) }
 
+        //Boton igual, calcular dependiendo las operaciones
         btnIgual.setOnClickListener(View.OnClickListener {
             val texto = lblPantalla.text
             var res: Double
             num2 = texto.toString().toDouble()
             when (operacion){
+                //Suma
                 1 -> {
                     res = (num1 + num2)
                     lblPantalla.text = res.toString()
                 }
+                //Resta
                 2 -> {
                     res = (num1 - num2)
                     lblPantalla.text = res.toString()
                 }
+                //Multiplicacion
                 3 -> {
                     res = (num1 * num2)
                     lblPantalla.text = res.toString()
                 }
+                //Division
                 4 -> {
+                    //Condicional para evitar la division entre 0
                     if (num2 == 0.0){
                         lblPantalla.text = "No se puede dividir entre cero"
                     }else{
@@ -111,9 +117,15 @@ class MainActivity : AppCompatActivity() {
                         lblPantalla.text = res.toString()
                     }
                 }
+                //Operacion Porcentaje
+                5 -> {
+                    res = (num1 * num2)/100
+                    lblPantalla.text = res.toString()
+                }
             }
         })
 
+        //Calcular Raiz
         btnRaiz.setOnClickListener( View.OnClickListener {
             var aux: Double = lblPantalla.text.toString().toDouble()
             if (aux >= 0){
@@ -122,16 +134,21 @@ class MainActivity : AppCompatActivity() {
             }
         })
 
+        //Calcular Inversa
         btnInversa.setOnClickListener( View.OnClickListener {
             var aux: Double = lblPantalla.text.toString().toDouble()
-            var inverso: Double = aux * 0.5
-            lblPantalla.text = inverso.toString()
+            //Condicional para evitar operacion inversa entre 0
+            if (aux == 0.0){
+                lblPantalla.text = "No se puede dividir entre cero"
+            }else{
+                var inverso: Double = 1 / aux
+                lblPantalla.text = inverso.toString()
+            }
         })
 
-        btnPorcentaje.setOnClickListener(View.OnClickListener {
-
-        })
     }
+
+    //Asignar valor a num1 y definir tipo de operacion
     private fun Operacion(op:Int) {
         val texto = lblPantalla.text
         num1 = texto.toString().toDouble()
@@ -139,14 +156,17 @@ class MainActivity : AppCompatActivity() {
         operacion = op
     }
 
+    //Funcion limpiar label
     private fun limpiar() {
         lblPantalla.text = "0"
     }
 
+    //Funcion para cambiar de signo
     private fun cambiarSigno() {
         val texto = lblPantalla.text
         val existe = '-' in texto
 
+        //Condicional para evitar el punto en el valor 0
         if(!(texto == "0")){
             if (existe){
                 lblPantalla.text = texto.substring(1)
@@ -156,17 +176,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    //Funcion para agregar el punto
     private fun agregarPunto() {
         val texto = lblPantalla.text
         val existe = '.' in texto
 
+        //Funcion para evitar repetir el "." si ya existe
         if (!existe){
             lblPantalla.text = "$texto"+"."
         }
     }
 
+    //Funcion para agregar numeros al label
     private fun agregarNum(num:String) {
         val texto = lblPantalla.text
+
+        //Condicional para reemplazar el mensaje de alerta de la divicion entre 0
         if (texto == "0" || texto == "No se puede dividir entre cero"){
             lblPantalla.text = "$num"
         }else{
