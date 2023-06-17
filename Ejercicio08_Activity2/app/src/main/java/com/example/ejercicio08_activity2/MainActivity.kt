721974bd1596d250cb17811174e9076472898cbb
AlejandroCamargoC <20211D115@khipu.edu.pe>
2023-06-16 20:01:18 -0500

    16/06

--- Ejercicio08_Activity2/app/src/main/java/com/example/ejercicio08_activity2/MainActivity.kt
+++ Ejercicio08_Activity2/app/src/main/java/com/example/ejercicio08_activity2/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.ejercicio08_activity2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun enviarDatos(view: View){
+        val intento = Intent(this, actDestino::class.java)
+        intento.putExtra("nombre", "Juan")
+        intento.putExtra("apPaterno", "Perez")
+        intento.putExtra("apMaterno", "Lopez")
+        intento.putExtra("edad", "33")
+        startActivity(intento)
     }
 }
---
+++ Ejercicio08_Activity2/app/src/main/java/com/example/ejercicio08_activity2/actDestino.kt
@@ -0,0 +1,32 @@
+package com.example.ejercicio08_activity2
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+
+class actDestino : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_act_destino)
+
+        val bundle = intent.getBundleExtra("intento")
+        val nombre = bundle?.getString("nombre")
+        val apPaterno = bundle?.getString("apPaterno")
+        val apMaterno = bundle?.getString("apMaterno")
+        val edad = bundle?.getString("edad")
+
+        val txtNombre = findViewById<EditText>(R.id.txtNombre)
+        val txtApPaterno = findViewById<EditText>(R.id.txtApPaterno)
+        val txtApMaterno = findViewById<EditText>(R.id.txtApMaterno)
+        val txtEdad = findViewById<EditText>(R.id.txtEdad)
+
+        txtNombre.setText(nombre)
+        txtApPaterno.setText(apPaterno)
+        txtApMaterno.setText(apMaterno)
+        txtEdad.setText(edad)
+        print(nombre)
+    }
+}
